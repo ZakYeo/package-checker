982aaa794f348b06e86d6ae5a826e2fb1baa6939
ZakYeo <zakyeomanson@googlemail.com>
2024-06-22 18:34:54 +0100

    Now can parse a package.json file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::fs;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PackageJson {
+    name: String,
+    version: String,
+    description: Option<String>,
+    main: Option<String>,
+    scripts: Option<HashMap<String, String>>,
+    dependencies: Option<HashMap<String, String>>,
+    dev_dependencies: Option<HashMap<String, String>>,
+    keywords: Option<Vec<String>>,
+    author: Option<String>,
+    license: Option<String>,
+    repository: Option<Repository>,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Repository {
+    #[serde(rename = "type")]
+    repo_type: String,
+    url: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_location = "./package.json"; 
+    let contents = fs::read_to_string(file_location)
+            .expect("Should have been able to read the file");
+
+    let parsed: Result<PackageJson, _> = serde_json::from_str(&contents);
+        match parsed {
+            Ok(pkg) => println!("Parsed package.json: {:?}", pkg),
+            Err(e) => println!("Failed to parse package.json: {}", e),
+        }
+
+
 }
